cde40f89388723c0f218ea535882c7aa0dc05521
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-08-30 23:41:00 -0400

    Added Joke Choice

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using Pastel;
+using System.Drawing;
+
 namespace ApiClient
 {
     class Joke
@@ -44,6 +47,34 @@ namespace ApiClient
                 }
             }
 
+            static async Task GetGeneralJoke()
+            {
+                var client = new HttpClient();
+                var url = "https://official-joke-api.appspot.com/jokes/general/random";
+                var responseAsStream = await client.GetStreamAsync(url);
+                var joke = await JsonSerializer.DeserializeAsync<List<Joke>>(responseAsStream);
+                Console.WriteLine($"\n{joke[0].SetUp} ~ ~ ~ {joke[0].PunchLine}");
+            }
+
+            static async Task GetProgrammingJoke()
+            {
+                var client = new HttpClient();
+                var url = "https://official-joke-api.appspot.com/jokes/programming/random";
+                var responseAsStream = await client.GetStreamAsync(url);
+                var joke = await JsonSerializer.DeserializeAsync<List<Joke>>(responseAsStream);
+                Console.WriteLine();
+                Console.WriteLine($"\n{joke[0].SetUp} ~ ~ ~ {joke[0].PunchLine}");
+            }
+
+            static async Task GetKnock_KnockJoke()
+            {
+                var client = new HttpClient();
+                var url = "https://official-joke-api.appspot.com/jokes/knock-knock/random";
+                var responseAsStream = await client.GetStreamAsync(url);
+                var joke = await JsonSerializer.DeserializeAsync<List<Joke>>(responseAsStream);
+                Console.WriteLine($"\n{joke[0].SetUp} ~ ~ ~ {joke[0].PunchLine}");
+            }
+
             static char PressAnyKey(string prompt)
             {
                 Console.WriteLine(prompt);
@@ -56,6 +87,7 @@ namespace ApiClient
                 Console.WriteLine("What would you like to do?");
                 Console.WriteLine("(R)andom Joke");
                 Console.WriteLine("(T)en Random Jokes");
+                Console.WriteLine("(J)oke Type");
                 Console.Write("(Q)uit\n: ");
                 var choice = Console.ReadLine().ToUpper();
                 Console.Clear();
@@ -81,9 +113,55 @@ namespace ApiClient
                             PressAnyKey("\nPress Any Key to Continue! ");
                             Console.Clear();
                             break;
+                        case "J":
+                            {
+
+                                var jokeType = true;
+                                while (jokeType)
+                                {
+                                    Console.Clear();
+                                    Console.Write("What type of joke would you like to hear?\n(G)eneral\n(P)rogramming\n(K)nock-knock\n: ");
+                                    var choice = Console.ReadLine().ToUpper();
+                                    switch (choice)
+                                    {
+                                        case "G":
+                                            Console.Clear();
+                                            await GetGeneralJoke();
+                                            PressAnyKey("\nPress Any Key to Continue! ");
+                                            Console.Clear();
+                                            jokeType = false;
+                                            break;
+                                        case "P":
+                                            Console.Clear();
+                                            await GetProgrammingJoke();
+                                            PressAnyKey("\nPress Any Key to Continue! ");
+                                            Console.Clear();
+                                            jokeType = false;
+                                            break;
+                                        case "K":
+                                            Console.Clear();
+                                            await GetKnock_KnockJoke();
+                                            PressAnyKey("\nPress Any Key to Continue! ");
+                                            Console.Clear();
+                                            jokeType = false;
+                                            break;
+                                        default:
+                                            Console.WriteLine($"\n{"Your answer was invalid. Please try again!".Pastel(Color.Red)}");
+                                            PressAnyKey("\nPress Any Key to Continue! ");
+                                            Console.Clear();
+                                            break;
+                                    }
+                                }
+                                break;
+                            }
                         case "Q":
                             Console.Clear();
                             keepTheJokesComing = false;
+                            break;
+                        default:
+                            Console.WriteLine($"{"Your answer was invalid. Please try again!".Pastel(Color.Red)}");
+                            PressAnyKey("\nPress Any Key to Continue! ");
+                            Console.Clear();
                             break;
                     }
                 }
